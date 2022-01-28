61fb32893fb9eaae9923699db28f4bae2bf8d111
shuva <chakrabortyshuva216@gmail.com>
2022-01-29 00:58:08 +0600

    nested_comperison i js commit

---
+++ nested_comperison.js
@@ -0,0 +1,20 @@
+var rice=40;
+var chicken=100;
+var money =200;
+var wellpacked=true;
+if (rice<money){
+    if(wellpacked==false)
+    {
+        console.log("i will buy rice");
+    }
+    else{
+        console.log("i will buy dall");
+    }
+    
+}
+else if (chicken>money){
+    console.log("i will buy chicken");
+}
+else{
+    console.log("go to home.")
+}
