f9aedb04f4a977a81dab725b7870813a72ffc7f2
shuva <chakrabortyshuva216@gmail.com>
2022-01-28 22:24:47 +0600

    how to add remove in array commit

---
+++ arraypushpop.js
@@ -0,0 +1,15 @@
+var lastseat=["Rahim","karim","shuva"];
+//add
+lastseat.push("sagor","kalam");
+console.log(lastseat);
+//remove
+lastseat.pop();
+console.log(lastseat);
+//shift(): Remove an item from the beginning of an array
+lastseat.shift();
+console.log(lastseat);
+//unshift() : Add items to the beginning of an array
+lastseat.unshift("rahul");
+console.log(lastseat);
+
+
