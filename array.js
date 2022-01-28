441b68f8a60634d1d499ca44a49e0b16da4c299d
shuva <chakrabortyshuva216@gmail.com>
2022-01-28 21:57:08 +0600

    array initial commit

---
+++ array.js
@@ -0,0 +1,13 @@
+var age=[10,20,30];
+console.log(age);
+//total numbers of length 
+console.log(age.length);
+//ex-2
+
+var name=["shuva","saiful","Tutul","Hridoy"];
+var indexofname= name.indexOf("Tutul");
+console.log(indexofname);
+var indexnumber =name[3];
+console.log(indexnumber);
+age[1]=900;
+console.log(age);
