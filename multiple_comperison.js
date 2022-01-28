e532957ec2ec73f4f3728994801e82f48d797b31
shuva <chakrabortyshuva216@gmail.com>
2022-01-29 00:37:14 +0600

    multiple comperison commit

---
+++ multiple_comperison.js
@@ -0,0 +1,18 @@
+var gotjob=true;
+var savedMoney=2000;
+var homeloan=500;
+/*if(gotjob=true && savedMoney==2000)
+{
+    console.log("You can buy a beautiful house")
+}
+else{
+    console.log("Try hard untill u success");
+}
+*/
+if((gotjob=true && savedMoney<2000) || homeloan==1000 )
+{
+    console.log("You can buy a beautiful house")
+}
+else{
+    console.log("Try hard untill u success");
+}
