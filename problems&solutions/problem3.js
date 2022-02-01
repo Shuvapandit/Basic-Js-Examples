87f76b394f22fa6b0278c5556da8c6a9b3e4228d
shuva <chakrabortyshuva216@gmail.com>
2022-02-02 00:52:56 +0600

    commit problem3 solution

--- problems&solutions/problem3.js
+++ problems&solutions/problem3.js
@@ -1,11 +1,11 @@
 /*তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে। */
 function agecalculation(age){
     if(age%2==0){
-        console.log("Your age is even and ")
+        console.log("Your age is even ")
         return true;
     }
     else {
-        console.log("Your age is  odd and")
+        console.log("Your age is  odd ")
         return false;
     }
 }
