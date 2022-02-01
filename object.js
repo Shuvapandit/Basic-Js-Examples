17bd7a4f9a99cf055ede87863d8703b77c839b4a
shuva <chakrabortyshuva216@gmail.com>
2022-02-01 10:22:36 +0600

    commit object in js code

---
+++ object.js
@@ -0,0 +1,12 @@
+var computer={
+    price:22000,
+    processor:"intel",
+    ram :4
+
+}
+console.log(computer);
+console.log(computer.price);
+var  computerram=computer.ram;
+console.log(computerram);
+
+computer.price=3000;
