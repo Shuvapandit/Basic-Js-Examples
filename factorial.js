b1a3d5919a00a6ff8f875bc5cad50263f5b6e248
shuva <chakrabortyshuva216@gmail.com>
2022-02-01 23:53:37 +0600

    count from back

--- factorial.js
+++ factorial.js
@@ -1,7 +1,34 @@
-var factorial=1;
+/*var factorial=1;
 for(var i=1;i<=7;i++){
    
 
     factorial=factorial*i;
 }
 console.log(factorial)
+
+function f(num){
+    let factr=1;
+    let i=1;
+    while(i<=num){
+        factr= factr * i;
+        i++;
+
+    }
+    return factr;
+}
+let ans =f(5);
+console.log(ans)
+*/
+
+function f(num){
+    let factr=1;
+    let i=num;
+    while(i>=1){
+        factr= factr * i;
+        i--;
+
+    }
+    return factr;
+}
+let ans =f(5);
+console.log(ans)
