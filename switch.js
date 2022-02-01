2ecfdc2fc16a46802a98ee83e1d764cbbf430e46
shuva <chakrabortyshuva216@gmail.com>
2022-02-01 10:39:03 +0600

    commit switch case in js

---
+++ switch.js
@@ -0,0 +1,33 @@
+var number=80;
+switch(number){
+    case 39:
+        {
+            console.log("you got B-");
+        }
+        break;
+    case 50:
+        {
+            console.log("you got B+");
+        }
+        break;
+    case 60:
+        {
+            console.log("you got A-");
+        }
+        break;
+    case 70:
+        {
+            console.log("you got A");
+        }
+        break;
+    case 80:
+        {
+            console.log("you got A+");
+        }
+        break;
+        default:
+            {
+                console.log("You failed");
+            }
+
+}
