cc0e020e3e28791f436aa69b37fdc70c4e8c68e9
shuva <chakrabortyshuva216@gmail.com>
2022-02-01 11:06:48 +0600

    commit break and continue in js

---
+++ break_continue.js
@@ -0,0 +1,28 @@
+var i =0;
+while(i<100){
+    console.log(i);
+    if(i==30){
+        
+        break;
+        
+    }
+   
+    i++;
+}
+//using for loop
+for (var x=0;x<100;x++)
+{
+    console.log(x)
+    if(x==10){
+        break;
+    }
+}
+
+//continue
+for (var x=0;x<30;x++)
+{
+    console.log(x)
+    if(x==10){
+        continue;
+    }
+}
