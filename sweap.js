07cc2c231f5efa0aa075dbb3f7be5b693435e11f
shuva <chakrabortyshuva216@gmail.com>
2022-02-02 19:09:55 +0600

    commit sweap numbers

---
+++ sweap.js
@@ -0,0 +1,13 @@
+let numb1=10;
+let numb2=20;
+console.log(numb1,numb2);
+/*
+let temp=numb1;
+numb1=numb2;
+numb2=temp;
+*/
+//destructuring
+[numb1,numb2]=[numb2,numb1]
+
+console.log(numb1,numb2)
+
