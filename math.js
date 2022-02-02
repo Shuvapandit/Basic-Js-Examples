f9e921a647866f472990bdf6f2d5e87463d8df9c
shuva <chakrabortyshuva216@gmail.com>
2022-02-02 18:34:11 +0600

    commit math in js

---
+++ math.js
@@ -0,0 +1,26 @@
+
+//absolute value
+var x=-6;
+let output=Math.abs(x);
+console.log(output);
+//ceilling value
+ var i=9.6000
+ let out= Math.ceil(i)
+
+ console.log(out);
+ let y=6
+ //let o=Math.floor(y)
+ //let o=Math.round(y)
+ //random value
+ for(z=1;z<=100;z++){
+    let o=Math.random(y)*100
+    let oout=Math.round(o)
+    console.log(oout)
+     
+ }
+ 
+
+ 
+
+ 
+
