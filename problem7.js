c210c0af232a351e5af696ed1c26a67b6bdac7af
shuva <chakrabortyshuva216@gmail.com>
2022-02-03 18:12:49 +0600

    commit problem 7 solution

---
+++ problem7.js
@@ -0,0 +1,16 @@
+/* একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে। */
+
+function bestFriend(array) {
+    var longestWord = "";
+  
+    array.forEach(function(word) {
+      if(word.length > longestWord.length) {
+        longestWord = word;
+      }
+    });
+  
+    return longestWord;
+  }
+  
+  var word = bestFriend(["shuva","Hridoy", "Anistutul"]);
+  console.log(word); // result is "jumped"
