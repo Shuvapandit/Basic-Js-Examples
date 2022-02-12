30e1a598c93d30f73632bc11296518c1306c5552
shuva <chakrabortyshuva216@gmail.com>
2022-02-13 00:16:00 +0600

    commit problem8 solutions

---
+++ problem8.js
@@ -0,0 +1,19 @@
+
+/*prob8 
+ তোমাকে ফাংশনের ইনপুট হিসেবে সেলসিয়াস দিবে। তুমি ক্যালকুলেশন করে তাপমাত্রা ফারেনহাইট এ কনভার্ট করে সেটার আউটপুট রিটার্ন করবে 
+*/
+function celsius(c){
+    let Fahrenheit=(c*9/5)+32;
+    return Fahrenheit;
+}
+const ferIs=celsius(5);
+console.log(ferIs,"°F");
+/* একইভাবে উল্টা হিসাব করবে। যাতে তোমাকে ফারেনহাইট হিসেবে তাপমাত্রা দিলে সেটাকে সেলসিয়াস এ কনভার্ট করে আউটপুট দিবে। 
+Celsius = (Fahrenheit - 32) * 5.0/9.0
+ */
+function Fahrenheit(f){
+    let celsius=(f - 32) * 5.0/9.0;
+    return celsius;
+}
+const celIs=Fahrenheit(10);
+console.log(celIs,"°c");
