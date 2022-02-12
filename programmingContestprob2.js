0200bf639e34e6f6e87072499d28cd7e9d01a7ce
shuva <chakrabortyshuva216@gmail.com>
2022-02-12 23:27:44 +0600

    commit programmingContestprob2 solution

---
+++ programmingContestprob2.js
@@ -0,0 +1,26 @@
+/* ২. [ ফাংশন নেম দিতে হবে pandaCost]: তিনটা প্যারামিটার লাগবে। 
+
+১ টি সিঙ্গারা এর দাম – ৭ টাকা
+
+১ টি সমুচা এর দাম – ১০ টাকা 
+
+১ টি জিলাপি এর দাম – ১৫ টাকা 
+
+এখন সে যদি বিভিন্ন সংখ্যার সিঙ্গারা, সমুচা, আর জিলাপি এর অর্ডার দেয় তাহলে টোটাল কত টাকা খরচ হবে হলো সেই সংখ্যা রিটার্ন করতে হবে।  */
+//:problem2
+function pandaCost(singara,sumucha,jilipi){
+    if(singara<=0 && sumucha <=0 && jilipi<=0){
+      let invalid="Please enter valid amount.";
+      return invalid;
+     
+    }
+    else{
+  
+      let totalprice= singara*7 /*taka */+sumucha*10 /*taka */ +jilipi*15 /*taka */;
+      return totalprice;
+     
+  
+    }
+  
+  } 
+  
