9d79b40e59a76957f16008d89566f01a1ea04923
shuva <chakrabortyshuva216@gmail.com>
2022-02-16 00:57:56 +0600

    commit case-convert.js

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+const firtstring="shuvA"
+const secondStrung="shuvA"
+if(firtstring.toLowerCase()==secondStrung.toLocaleLowerCase()){
+    console.log('matched')
+}
+else{
+    console.log('not matched')
+}
