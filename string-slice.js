b9050dcbb9240eb774d48363f1e6d74166699e29
shuva <chakrabortyshuva216@gmail.com>
2022-02-16 15:43:24 +0600

    commit split ,slice,substring,concat and join method in js

---
+++ string-slice.js
@@ -0,0 +1,16 @@
+/* const anthem="Amr sonar Bangla Ami tmy valobashi";
+const words=anthem.split(' ');
+console.log(words)
+const wordslice=anthem.slice(4,16)
+console.log(wordslice);
+const anthrPart=anthem.substring(2,4)
+console.log(anthrPart); */
+///concat
+const a="Chandpur,";
+const b="city";
+const ab=a.concat(b).concat(' ' ,"of joy");
+console.log(ab);
+//join 
+const words=['c','h','a','n','d','p','u','r'];
+const fullwordjoined=words.join('');
+console.log(fullwordjoined);
